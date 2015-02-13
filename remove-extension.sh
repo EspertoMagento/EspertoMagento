1844e45ed2e76372488fe4f6188d87ab9f6b7ff7
Daniele Crupi <danycrup@gmail.com>
2015-02-13 12:48:01 +0100

    version 1.2

--- remove-extension.sh
+++ remove-extension.sh
@@ -6,27 +6,25 @@ echo -e "----------------\n"
 
 find . | grep -i $1 | nl
 
-if [ $(find . | grep -i $1) ]; then
+if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
 	
-echo -e "\nWant you remove files? [Y/n] "
-read remove || exit 1
-  case "$remove" in
-  [Nn]*) remove=n with=out;;
-  *)     remove=y with=;;
-  esac
+	echo -e "\nWant you remove files? [Y/n] "
+	read remove || exit 1
+  	case "$remove" in
+  	[Nn]*) remove=n with=out;;
+  	*)     remove=y with=;;
+  	esac
 
-if [ 'y' = $remove ]; then
-	echo -e "\nRemoving files and directories....\n"
-	find . | grep -i $1 | xargs -I file rm -frv file
-	echo -e "\nRemove Complete!\n"
+	if [ 'y' = $remove ]; then
+		echo -e "\nRemoving files and directories....\n"
+		find . | grep -i $1 | xargs -I file rm -frv file
+		echo -e "\nRemove Complete!\n"
+	else
+		echo -e "\nNothing to remove.\n\n"
+	fi
+
 else
-	echo -e "\nNothing to remove.\n\n"
-fi
-
-else
-
 	echo -e "\nNothing found.\n"
-
 fi
 
 echo -e "----------------------------------"
--- search-extension.sh
+++ search-extension.sh
@@ -3,22 +3,21 @@
 echo -e "\n----------------"
 echo -e "LIST FILES FOUND"
 echo -e "----------------\n"
+
 find . | grep -i $1 | nl
 
-if [ $(find . | grep -i $1) ]; then
+if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
 
-if [ -n "$2" ]; then
-	echo -e "\nCopying files and directories....\n"
-	find . | grep -i $1 | xargs -I file cp -v -r --parents file --target-directory=$2
-	echo -e "\nCopy Complete!\n"
+	if [ -n "$2" ]; then
+		echo -e "\nCopying files and directories....\n"
+		find . | grep -i $1 | xargs -I file cp -v -r --parents file --target-directory=$2
+		echo -e "\nCopy Complete!\n"
+	else
+		echo -e "\nNothing to copy.\n\n"
+	fi
+
 else
-	echo -e "\nNothing to copy.\n\n"
-fi
-
-else
-
 	echo -e "\nNothing found.\n"
-
 fi
 
 echo -e "----------------------------------"
