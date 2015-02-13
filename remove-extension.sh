c83c0166b01f1abfd1205f86540b0c76a32de704
Daniele Crupi <danycrup@gmail.com>
2015-02-13 12:37:34 +0100

    version 1.0

---
+++ remove-extension.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo -e "\n----------------"
+echo -e "LIST FILES FOUND"
+echo -e "----------------\n"
+
+find . | grep -i $1 | nl
+
+if [ $(find . | grep -i $1) ]; then
+	
+echo -e "\nWant you remove files? [Y/n] "
+read remove || exit 1
+  case "$remove" in
+  [Nn]*) remove=n with=out;;
+  *)     remove=y with=;;
+  esac
+
+if [ 'y' = $remove ]; then
+	echo -e "\nRemoving files and directories....\n"
+	find . | grep -i $1 | xargs -I file rm -frv file
+	echo -e "\nRemove Complete!\n"
+else
+	echo -e "\nNothing to remove.\n\n"
+fi
+
+else
+
+	echo -e "\nNothing found.\n"
+
+fi
+
+echo -e "----------------------------------"
+echo -e "THE SEARCH IS ENDED. THANK YOU! :)"
+echo -e "----------------------------------\n"
+exit;
