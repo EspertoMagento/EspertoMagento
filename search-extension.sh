db39bc295cb8f2c20ed8e3339cf0fb9ce58bc88a
EspertoMagento <danycrup@gmail.com>
2016-03-04 11:34:37 +0100

    Update search-extension.sh

--- search-extension.sh
+++ search-extension.sh
@@ -4,21 +4,41 @@ echo -e "\n----------------"
 echo -e "LIST FILES FOUND"
 echo -e "----------------\n"
 
-find . | grep -i $1 | nl
+if [ "" == "$2" ]; then
+        find . | grep -i $1 | nl
+        if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
 
-if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
+                if [ -n "$2" ]; then
+                        echo -e "\nCopying files and directories....\n"
+                        find . | grep -i $1 | xargs -I file cp -v -r --parents file --target-directory=$2
+                        echo -e "\nCopy Complete!\n"
+                else
+                        echo -e "\nNothing to copy.\n\n"
+                fi
 
-	if [ -n "$2" ]; then
-		echo -e "\nCopying files and directories....\n"
-		find . | grep -i $1 | xargs -I file cp -v -r --parents file --target-directory=$2
-		echo -e "\nCopy Complete!\n"
-	else
-		echo -e "\nNothing to copy.\n\n"
-	fi
+        else
+                echo -e "\nNothing found.\n"
+        fi
 
 else
-	echo -e "\nNothing found.\n"
+
+	find $2 | grep -i $1 | nl
+
+	if [ $(find $2 | grep -i $1 | wc -l) -gt 0 ]; then
+
+        	if [ -n "$3" ]; then
+                	echo -e "\nCopying files and directories....\n"
+                	find $2 | grep -i $1 | xargs -I file cp -v -r --parents file --target-directory=$3
+                	echo -e "\nCopy Complete!\n"
+        	else
+                	echo -e "\nNothing to copy.\n\n"
+        	fi
+
+	else
+        	echo -e "\nNothing found.\n"
+	fi
 fi
+
 
 echo -e "----------------------------------"
 echo -e "THE SEARCH IS ENDED. THANK YOU! :)"
