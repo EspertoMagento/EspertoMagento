9e9b68eb02902c866f49040720ce1994259df09c
EspertoMagento <danycrup@gmail.com>
2016-03-04 11:40:58 +0100

    Update remove-extension.sh

--- remove-extension.sh
+++ remove-extension.sh
@@ -4,27 +4,55 @@ echo -e "\n----------------"
 echo -e "LIST FILES FOUND"
 echo -e "----------------\n"
 
-find . | grep -i $1 | nl
+if [ "" == "$2" ]; then
 
-if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
+	find . | grep -i $1 | nl
+
+	if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
 	
-	echo -e "\nWant you remove files? [Y/n] "
-	read remove || exit 1
-  	case "$remove" in
-  	[Nn]*) remove=n with=out;;
-  	*)     remove=y with=;;
-  	esac
+		echo -e "\nWant you remove files? [Y/n] "
+		read remove || exit 1
+  		case "$remove" in
+  		[Nn]*) remove=n with=out;;
+  		*)     remove=y with=;;
+  		esac
 
-	if [ 'y' = $remove ]; then
-		echo -e "\nRemoving files and directories....\n"
-		find . | grep -i $1 | xargs -I file rm -frv file
-		echo -e "\nRemove Complete!\n"
+		if [ 'y' = $remove ]; then
+			echo -e "\nRemoving files and directories....\n"
+			find . | grep -i $1 | xargs -I file rm -frv file
+			echo -e "\nRemove Complete!\n"
+		else
+			echo -e "\nNothing to remove.\n\n"
+		fi
+
 	else
-		echo -e "\nNothing to remove.\n\n"
+		echo -e "\nNothing found.\n"
 	fi
 
 else
-	echo -e "\nNothing found.\n"
+	find $2 | grep -i $1 | nl
+
+	if [ $(find $2 | grep -i $1 | wc -l) -gt 0 ]; then
+	
+		echo -e "\nWant you remove files? [Y/n] "
+		read remove || exit 1
+  		case "$remove" in
+  		[Nn]*) remove=n with=out;;
+  		*)     remove=y with=;;
+  		esac
+
+		if [ 'y' = $remove ]; then
+			echo -e "\nRemoving files and directories....\n"
+			find $2 | grep -i $1 | xargs -I file rm -frv file
+			echo -e "\nRemove Complete!\n"
+		else
+			echo -e "\nNothing to remove.\n\n"
+		fi
+
+	else
+		echo -e "\nNothing found.\n"
+	fi
+
 fi
 
 echo -e "----------------------------------"
