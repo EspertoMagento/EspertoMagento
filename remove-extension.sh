1a1aeeaf9040a8a5fe575d46d85a44a1a806fe9b
EspertoMagento <danycrup@gmail.com>
2018-11-23 11:58:59 +0100

    fix bug

--- remove-extension.sh
+++ remove-extension.sh
@@ -6,9 +6,9 @@ echo -e "----------------\n"
 
 if [ "" == "$2" ]; then
 
-	find . | grep -i $1 | nl
+	find . | grep -ilr $1 . | nl
 
-	if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
+	if [ $(find . | grep -ilr $1 . | wc -l) -gt 0 ]; then
 	
 		echo -e "\nWant you remove files? [Y/n] "
 		read remove || exit 1
@@ -19,7 +19,7 @@ if [ "" == "$2" ]; then
 
 		if [ 'y' = $remove ]; then
 			echo -e "\nRemoving files and directories....\n"
-			find . | grep -i $1 | xargs -I file rm -frv file
+			find . | grep -ilr $1 . | xargs -I file rm -frv file
 			echo -e "\nRemove Complete!\n"
 		else
 			echo -e "\nNothing to remove.\n\n"
@@ -30,9 +30,9 @@ if [ "" == "$2" ]; then
 	fi
 
 else
-	find $2 | grep -i $1 | nl
+	find $2 | grep -ilr $1 $2 | nl
 
-	if [ $(find $2 | grep -i $1 | wc -l) -gt 0 ]; then
+	if [ $(find $2 | grep -ilr $1 $2 | wc -l) -gt 0 ]; then
 	
 		echo -e "\nWant you remove files? [Y/n] "
 		read remove || exit 1
@@ -43,7 +43,7 @@ else
 
 		if [ 'y' = $remove ]; then
 			echo -e "\nRemoving files and directories....\n"
-			find $2 | grep -i $1 | xargs -I file rm -frv file
+			find $2 | grep -ilr $1 $2 | xargs -I file rm -frv file
 			echo -e "\nRemove Complete!\n"
 		else
 			echo -e "\nNothing to remove.\n\n"
