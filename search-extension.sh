fba8660b34a1c6b29fb4889b33242c4e9bf338b9
EspertoMagento <danycrup@gmail.com>
2018-11-23 11:55:17 +0100

    fix bug

--- search-extension.sh
+++ search-extension.sh
@@ -5,12 +5,12 @@ echo -e "LIST FILES FOUND"
 echo -e "----------------\n"
 
 if [ "" == "$2" ]; then
-        find . | grep -i $1 | nl
-        if [ $(find . | grep -i $1 | wc -l) -gt 0 ]; then
+        find . | grep -ilr $1 . | nl
+        if [ $(find . | grep -ilr $1 . | wc -l) -gt 0 ]; then
 
                 if [ -n "$2" ]; then
                         echo -e "\nCopying files and directories....\n"
-                        find . | grep -i $1 | xargs -I file cp -v -r --parents file --target-directory=$2
+                        find . | grep -ilr $1 . | xargs -I file cp -v -r --parents file --target-directory=$2
                         echo -e "\nCopy Complete!\n"
                 else
                         echo -e "\nNothing to copy.\n\n"
@@ -22,13 +22,13 @@ if [ "" == "$2" ]; then
 
 else
 
-	find $2 | grep -i $1 | nl
+	find $2 | grep -ilr $1 $2 | nl
 
-	if [ $(find $2 | grep -i $1 | wc -l) -gt 0 ]; then
+	if [ $(find $2 | grep -ilr $1 $2 | wc -l) -gt 0 ]; then
 
         	if [ -n "$3" ]; then
                 	echo -e "\nCopying files and directories....\n"
-                	find $2 | grep -i $1 | xargs -I file cp -v -r --parents file --target-directory=$3
+                	find $2 | grep -ilr $1 $2 | xargs -I file cp -v -r --parents file --target-directory=$3
                 	echo -e "\nCopy Complete!\n"
         	else
                 	echo -e "\nNothing to copy.\n\n"
