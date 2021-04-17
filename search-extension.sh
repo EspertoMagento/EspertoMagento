80cc3730cd9153ee99f9927ed10b90d8c3782ba0
Daniele Crupi <danycrup@gmail.com>
2021-04-17 16:29:12 +0200

    fixes

--- search-extension.sh
+++ search-extension.sh
@@ -6,11 +6,13 @@
 Help()
 {
    # Display Help
+   echo
    echo "This script allows you to search an extension within a particular installation of Magento and retrieve the entire list of files. It also allows you to copy files and directories found within a folder."
    echo
-   echo "Usage: ./search-extension.sh -n [extensions_to_search] -s [source_directory] -t [target_directory] [-h]"
+   echo "Usage: ./search-extension.sh [-n|--name] extensions_to_search [-s|--source] source_directory [-t|--target] target_directory [-h]"
+   echo
    echo "options:"
-   echo "-h|--help       	   	           Print this Help."
+   echo "-h|--help       	   	    Print this Help."
    echo "-n|--name extensions_to_search     Set the name of module to search."
    echo "-s|--source source_directory       Set source directory to search files."
    echo "-t|--target target_directory       Set directory to copy files."
@@ -31,7 +33,7 @@ while getopts ":h" option; do
    esac
 done
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "" ]; then
     Help
 fi
 
