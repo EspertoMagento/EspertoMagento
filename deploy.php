97abb6858be4a5203e56acc818c456a44def8a98
Daniele Crupi <danycrup@gmail.com>
2021-04-23 17:45:01 +0200

    new commit

--- deploy.php
+++ deploy.php
@@ -3,9 +3,14 @@ namespace Deployer;
 
 require 'recipe/common.php';
 
-task('deploy:staging', function() {
-    writeln('<info>Deploying...</info>');
-    #run('git pull');
+task('deploy:clone', function() {
+    writeln('<info>Clone Repository...</info>');
+    run('cd {{app_dir}}/ && git clone {{repository}}');
+});
+
+task('deploy:pull', function() {
+    writeln('<info>Pull repository...</info>');
+    run('cd {{app_dir}}/{{application}} && git pull');
 });
 
 // Project name
@@ -17,15 +22,18 @@ set('app_dir', '/home/deploy/ftp/scripts');
 // Project repository
 set('repository', 'https://github.com/EspertoMagento/EspertoMagento.git');
 
+// Local path
+set('local_path', '/Volumes/MacintoshHD/Library/WebServer/Documents/Espertomagento');
+
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
 
 // Shared files/dirs between deploys 
-set('shared_files', []);
-set('shared_dirs', []);
+//set('shared_files', []);
+//set('shared_dirs', []);
 
 // Writable dirs by web server 
-set('writable_dirs', []);
+//set('writable_dirs', []);
 
 task('current_path', function () {
     return run('pwd');
@@ -39,13 +47,37 @@ host('kontabo')
     
 host('localhost')
     ->stage('dev')
-    ->set('deploy_path', '/Volumes/MacintoshHD/Library/WebServer/Documents/EspertoMagento');
+    ->set('deploy_path', '/Volumes/MacintoshHD/Library/WebServer/Documents/EspertoMagento')
+    ->identityFile('~/.ssh/id_rsa');
 
 // Tasks
-task( 'deploy:link', function () {
+task( 'deploy:link', function() {
     run( 'ln -sfn {{deploy_path}}/current {{app_dir}}/{{application}}' );
 } );
 
+
+task ('local:prepare', function(){
+    writeln('<info>Change directory to Local app...</info>');
+    run('cd {{local_path}}'); 
+});
+
+task('local:push', function() {
+    //run('cd {{current_path}}');
+    writeln('<info>Add files to repository...</info>');
+    run('git add -u');
+    writeln('<info>Commit files to repository...</info>');
+    run('git commit -m "new commit" && git push');
+    writeln('<info>Push completed!</info>');
+});
+
+
+desc('Deploy on local');
+task('local', [
+    'local:prepare',
+    'local:push'
+]); 
+
+set('release_path', "{{app_dir}}");
 desc('Deploy your project');
 task('deploy', [
     'deploy:info',
@@ -53,11 +85,11 @@ task('deploy', [
     'deploy:lock',
     'deploy:release',
     'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
+    //'deploy:shared',
+    //'deploy:writable',
     //'deploy:vendors',
     'deploy:clear_paths',
-    'deploy:symlink',
+    //'deploy:symlink',
     'deploy:link',
     'deploy:unlock',
     'cleanup',
